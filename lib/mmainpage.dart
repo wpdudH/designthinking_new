11a84eb0ea44d6324f863a13a862a371e0ad283b
mmindong <san012@khu.ac.kr>
2020-12-16 05:26:28 +0900

    [ADD] mainpage.dart

---
+++ lib/mmainpage.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import './ReviseHome.dart';
+import 'data_move.dart';
+import 'mainpage.dart';
+
+class DocumentView1 extends StatefulWidget {
+  @override
+  DocumentView1State createState() => DocumentView1State();
+}
+
+class DocumentView1State extends State<DocumentView1> {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  Stream<QuerySnapshot> currentStream;
+  List<String> nameList = [];
+
+  void initState() {
+    super.initState();
+    firestore.collection("User").get().then((QuerySnapshot querySnapshot) => {
+          querySnapshot.docs.forEach((doc) {
+            nameList.add(doc["name"]);
+          })
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Stack(children: <Widget>[
+        // Adobe XD layer: 'fixed' (group)
+
+        Container(
+            child: nameList != null
+                ? Container(
+                    padding: EdgeInsets.fromLTRB(0, 150, 0, 0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          imageList(nameList),
+                        ]),
+                  )
+                : Container()),
+        Transform.translate(
+          offset: Offset(0.0, 156.0),
+          child:
+              // Adobe XD layer: '27088-[Converted]' (shape)
+              Container(
+            width: 401.0,
+            height: 425.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: const AssetImage('assets/images/Refrigerator.png'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ),
+        Transform.translate(
+          offset: Offset(140.6, 100.0),
+          child: FlatButton(
+            color: const Color(0xff404E5A),
+            child: Text(
+              'Register',
+              style: TextStyle(
+                fontFamily: 'Acumin Pro SemiCondensed',
+                fontSize: 16,
+                color: const Color(0xffffffff),
+                letterSpacing: -0.16,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            onPressed: () {},
+          ),
+        ),
+        Container(),
+        Container(),
+        Container(),
+      ]),
+    );
+  }
+  //document를 ingredient로 묶음
+  //document -> name[0], name[1] 을 위한 List 필요함.-> doc(name[i]).field 읽도록 함.
+
+  Widget imageList(List<String> nameList) {
+    int lstLength = nameList.length;
+    if (lstLength <= 5) {
+      return Container(
+        height: 80,
+        padding: EdgeInsets.symmetric(vertical: 15),
+        child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: makeImageBar(context, lstLength, nameList)),
+      );
+    }
+    return Text("d");
+  }
+
+  List<Widget> makeImageBar(
+      BuildContext context, int lstLength, List<String> nameList) {
+    List<Widget> results = [];
+    for (int i = 0; i < lstLength; i++) {
+      results.add(InkWell(
+        child: Image.network(
+            'http://ch4n3.kr:3000/image?keyword=' + nameList[i],
+            width: 60,
+            height: 60,
+            fit: BoxFit.contain),
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ReviseHome(nameList[i])));
+        },
+      ));
+    }
+    return results;
+  }
+}
