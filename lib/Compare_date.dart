f7f3d7141f0f26fcfb4d2c82ff50c2a6ce2e3e00
wpdudH <wpdudghd@gmail.com>
2020-12-17 04:28:16 +0900

    [fix] - add print d-day function

--- lib/Compare_date.dart
+++ lib/Compare_date.dart
@@ -2,3 +2,10 @@ void sortDay(List<String> timeTable)
 {
   timeTable.sort((a,b) => a.compareTo(b));
 }
+
+String ComapreDate(String time){
+  DateTime Time = DateTime.parse(time);
+  DateTime date2 = DateTime.now();
+  int difference = Time.difference(date2).inDays;
+  return difference.toString();
+}
